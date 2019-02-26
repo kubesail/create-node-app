d357d8342aa49e9066b12e08e4ac0e3039e2f34b
Dan Pastusek <dan@hexial.com>
2019-02-25 17:27:55 -0800

    copy templates

---
+++ src/template/src/www/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import logoReact from './logo-react.svg'
+import logoKube from './logo-kube.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logoReact} className="App-logo" alt="logo" />
+          <img src={logoKube} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/www/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
