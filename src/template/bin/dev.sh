2666dbb3df4effa0d7c0dcec7db6c3be050b47b7
Dan Pastusek <dan@hexial.com>
2019-04-27 16:13:51 -0700

    concurrently --kill-others, move required dependencies for npx

--- src/template/bin/dev.sh
+++ src/template/bin/dev.sh
@@ -9,6 +9,7 @@
   --names "www,api" \
   --handle-input true \
   --default-input-target 1 \
+  --kill-others \
   --prefix-colors "bgBlue.bold,bgMagenta.bold" \
   "bash ./bin/dev_www.sh" \
   "bash ./bin/dev_api.sh"
