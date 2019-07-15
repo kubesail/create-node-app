9437fbabd8f1ac7c32554dc6eab5509d01c3f5d1
Dan Pastusek <dan@hexial.com>
2019-07-14 21:35:41 -0700

    fix sample api template

--- src/template/src/api/index.js
+++ src/template/src/api/index.js
@@ -11,15 +11,15 @@ postgres.query(`CREATE TABLE IF NOT EXISTS clicks (
 )`)
 
 app.get('/api/count', (req, res) => {
-  postgres.query('SELECT count(*) AS count FROM clicks', (err, count) => {
-    res.send({ count: count || 0 })
+  postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
+    res.send({ count: resp.rows[0].count || 0 })
   })
 })
 
 app.post('/api/count/increment', (req, res) => {
-  postgres.query('INSERT INTO clicks DEFAULT VALUES', (err, resp) => {
-    postgres.query('SELECT count(*) AS count FROM clicks', (err, count) => {
-      res.send({ count: count || 0 })
+  postgres.query('INSERT INTO clicks DEFAULT VALUES', (err, insert) => {
+    postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
+      res.send({ count: resp.rows[0].count || 0 })
     })
   })
 })
