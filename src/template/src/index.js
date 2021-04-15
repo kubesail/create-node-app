7348b45546e40a105e4a640bc0fa3cd8020ab401
mdeiters <miked@drivevariant.com>
2021-04-15 12:59:45 -0400

    delete old template

--- src/template/src/index.js
+++
@@ -1,6 +0,0 @@
-// "Create React App" tooling expects to find the WWW root here,
-// so we simply use this file to import the ./www directory.
-//
-// For the backend, we set the container's entrypoint to src/api/index.js
-
-module.exports = require('./www')
